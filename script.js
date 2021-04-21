56b93f54b31689f768f9967739ed8e2b3ca87630
Khadija Ladan <khadijaladan67@gmail.com>
2021-04-21 12:57:32 +0100

    y=try

--- script.js
+++ script.js
@@ -21,18 +21,3 @@ const closeSidebar = () => {
 }
 
 window.addEventListener("scroll", changeToSolidNav);
-
-// const sendEmail = () => { 
-//   Email.send({ 
-//     Host: "smtp.gmail.com", 
-//     Username: "sender@email_address.com", 
-//     Password: "Enter your password", 
-//     To: 'receiver@email_address.com', 
-//     From: "sender@email_address.com", 
-//     Subject: "Sending Email using javascript", 
-//     Body: "Well that was easy!!", 
-//   }) 
-//     .then(function (message) { 
-//       alert("mail sent successfully") 
-//     }); 
-// } 
